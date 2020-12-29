4ab16ea319216b46afd270c5d9db780b436279c8
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-29 15:59:30 -0300

    Adicionando Validações

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,9 +1,10 @@
 <template>
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
-    <v-card class="blue darken-4 darker-3 white--text">
+    <v-card class="primary darker-3 white--text">
       <v-card-title class="headline">
         <strong
-          >{{ stock.name }} <small>(Preço: {{ stock.price }} | Qtde: {{stock.quantity}})</small>
+          >{{ stock.name }}
+          <small>(Preço: {{ stock.price }} | Qtde: {{ stock.quantity }})</small>
         </strong>
       </v-card-title>
     </v-card>
@@ -12,21 +13,25 @@
         <v-text-field
           label="Quantidade"
           type="number"
+          :error="insufficientQuantity || !Number.isInteger(quantity)"
           v-model.number="quantity"
         ></v-text-field>
         <v-btn
-          class="blue darken-4 darken-3 white--text"
-          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          class="primary darken-3 white--text"
+          :disabled="
+            insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)
+          "
           @click="sellStock"
-          >Venda</v-btn
         >
+          {{insufficientQuantity ? 'Insuficiente' : 'Vender'}}
+        </v-btn>
       </v-container>
     </v-card>
   </v-flex>
 </template>
 
 <script>
-import {mapActions} from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
   props: ["stock"],
@@ -35,8 +40,13 @@ export default {
       quantity: 0
     };
   },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
+    }
+  },
   methods: {
-      ...mapActions({sellStockAction: "sellStock"}),
+    ...mapActions({ sellStockAction: "sellStock" }),
     sellStock() {
       const order = {
         stockId: this.stock.id,
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -12,10 +12,15 @@
         <v-text-field
           label="Quantidade"
           type="number"
+          :error="insufficientFunds || !Number.isInteger(quantity)"
           v-model.number="quantity"
         ></v-text-field>
-        <v-btn class="green darken-3 white--text" 
-        :disabled="quantity <= 0 || !Number.isInteger(quantity)" @click="buyStock">Comprar</v-btn>
+        <v-btn
+          class="green darken-3 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity) || insufficientFunds"
+          @click="buyStock"
+          > {{insufficientFunds ? 'Insuficiente' : 'Comprar'}} </v-btn
+        >
       </v-container>
     </v-card>
   </v-flex>
@@ -29,6 +34,14 @@ export default {
       quantity: 0
     };
   },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
+    }
+  },
   methods: {
     buyStock() {
       const order = {
@@ -36,7 +49,7 @@ export default {
         stockPrice: this.stock.price,
         quantity: this.quantity
       };
-      this.$store.dispatch('buyStock', order);
+      this.$store.dispatch("buyStock", order);
       this.quantity = 0;
     }
   }
