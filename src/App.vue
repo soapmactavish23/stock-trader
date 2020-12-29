6906933e1980f35342b118c4a08be50fce33d9f2
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-29 11:04:38 -0300

    Criando o componente de Ações

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-	<v-app>
-		<Header></Header>
-		<v-spacer></v-spacer>
-		<v-container>
-			<router-view></router-view>
-		</v-container>
-	</v-app>
+  <v-app>
+    <Header></Header>
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
 export default {
-	components: {Header}
-}
+  components: { Header }
+};
 </script>
 
 <style>
-
 </style>
 
