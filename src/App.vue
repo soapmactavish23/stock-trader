16acfa4d17296a05d29cb6b06be82a93ca9df8a2
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-29 11:44:31 -0300

    Configurando o Vuex no Projeto

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,10 @@
 <script>
 import Header from "./components/Header.vue";
 export default {
-  components: { Header }
+  components: { Header },
+  created() {
+    this.$store.dispatch("initStocks");
+  }
 };
 </script>
 
