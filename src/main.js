1f9d48188abf089f5b9008a110c516ac6c8f9ffe
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-30 09:43:03 -0300

    Configurando Axios & Firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
-import './plugins/vuetify';
 import App from './App.vue';
+
+import './plugins/vuetify';
+import './plugins/axios';
+
 import router from './router';
 import store from './store/store';
 
