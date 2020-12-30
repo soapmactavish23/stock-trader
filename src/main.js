27c86b20439c90a6270411b5baf7c9ddd9dbbd22
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-30 09:06:10 -0300

    Aplicando Filtro de Valor Monetário no Saldo

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store/store';
 
 Vue.config.productionTip = false;
 
+// Filtro
+Vue.filter('currency', value => {
+	return 'R$' + value.toLocaleString();
+});
+
 new Vue({
 	router,
 	store,
